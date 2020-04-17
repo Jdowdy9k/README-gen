c6b451484733bb9baacc6678f4df53bdd0512bd8
Jdowdy <Jdowdy9k@gamil.com>
2020-04-17 18:55:49 -0400

    working on appending file

--- index.js
+++ index.js
@@ -2,101 +2,108 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your Github username?",
-            name: "gitName"
-        },
-        {
-            type: "input",
-            message: "What is the title of the project? (this should be the same name as repo)",
-            name: "proName"
-        },
-        {
-            type: "input",
-            message: "Give a discription of your project.",
-            name: "proDesc"
-        },
-        {
-            type: "input",
-            message: "Give a table of contents for your project.",
-            name: "proTable"
-        },
-        {
-            type: "input",
-            message: "How would the user install your project?",
-            name: "proInstall"
-        },
-        {
-            type: "input",
-            message: "How would the user use your project?",
-            name: "proUse"
-        },
-        {
-            type: "input",
-            message: "What licenses are used in your project?",
-            name: "prolicenses"
-        },
-        {
-            type: "input",
-            message: "who is Contributing to your project?",
-            name: "proContr"
-        },
-        {
-            type: "input",
-            message: "What tests are used in your project?",
-            name: "proTest"
-        },
-        
-    ]).then(function (questions) {
+function fireQuestions() {
+    return inquirer.prompt(questions)
+}
+
+const questions = [
+
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "gitName"
+    },
+    {
+        type: "input",
+        message: "What is your E-mail?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the title of the project? (this should be the same name as repo)",
+        name: "proName"
+    },
+    {
+        type: "input",
+        message: "Give a discription of your project.",
+        name: "proDesc"
+    },
+    {
+        type: "input",
+        message: "Give a table of contents for your project.",
+        name: "proTable"
+    },
+    {
+        type: "input",
+        message: "How would the user install your project?",
+        name: "proInstall"
+    },
+    {
+        type: "input",
+        message: "How would the user use your project?",
+        name: "proUse"
+    },
+    {
+        type: "input",
+        message: "What licenses are used in your project?",
+        name: "prolicenses"
+    },
+    {
+        type: "input",
+        message: "who is Contributing to your project?",
+        name: "proContr"
+    },
+    {
+        type: "input",
+        message: "What tests are used in your project?",
+        name: "proTest"
+    },
 
 
+]
+function init() {
 
+    fireQuestions()
+        .then(function (questions) {
 
-      fs.writeFile("README.md", generateMarkdown = `
-            # ${proName.response} README \n
-            ## **${gitName.response}** \n
-            # User Profile ${response.data.avatar_url} \n
+            fs.writeFile("README.md", function (err) {
+                `
+            # ${ proName.response} README \n
+            ## ** ${ gitName.response}** \n
+            ## ** Email **\n
+                ${ email.response} \n
+            # User Profile ${ response.data.avatar_url} \n
             # App Description \n
-            ${proDesc.response}\n
+                ${ proDesc.response} \n
             ## Table of Contents \n
-            ${proTable.response}\n
+                ${ proTable.response} \n
             ### Installation \n
-            ${proInstall.response}\n
-            ## ** Email**\n
-            ${.}\n
+                ${ proInstall.response} \n
             ## Contributors \n
-            ${.}\n
+                ${ proContr.response} \n
             ## Testing \n
-            ${.}\n
-            ## Questions \n
-            ${.}
-            `,
-            
+                ${ protest.response} \n`
 
-            function(err) {
 
-                if (err) {
-                  return console.log(err);
-                }
-            
-            
-                })
-               let badge = "[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]()";
-                fs.appendFile("README.md", '\n' + badge, function(err){
+
+               
+                let badge = "[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]()";
+                fs.appendFile("README.md", '\n' + process.argv[2] + badge, function (err) {
                     if (err) {
                         return console.log(err);
                     }
-                    else{
+                    else {
                         console.log("Success! You did it!");
                     }
                 });
 
+
+
             });
+
         });
-    };
+
+}
 
 init();
 
